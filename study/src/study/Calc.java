77ff8acd94aa36711b4c11a24a3c352ec46bdaad
rmdwjd <136539496+rmdwjd@users.noreply.github.com>
2023-12-15 10:19:02 +0900

    20231215

---
+++ study/src/study/Calc.java
@@ -0,0 +1,19 @@
+package study;
+
+import java.util.Scanner;
+
+public class Calc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.print("첫번쨰 숫자 입력하기 :  ");
+		int num1 = sc.nextInt();
+		System.out.print("두번째 정수 입력하기 : ");
+		int num2 = sc.nextInt();
+		System.out.print("연산자 입력하기 (+, -, *, /) : ");
+		char op = sc.next();
+		
+	}
+
+}
